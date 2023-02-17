3cfa79d19e4125a16c8ca861110cc3bfbe7559bd
victor <victor.buthia@student.moringaschool.com>
2023-02-17 20:51:55 +0300

    completed

---
+++ lib/create.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bears (id INTEGER, name TEXT, age INTEGER, sex TEXT, color TEXT, temperament TEXT, alive BOOLEAN);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr.Chorcolate', 38, 'M', 'black', 'optimistic', 'true'); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 30, 'M', 'black', 'relaxed', 'true');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 28, 'F', 'white', 'analytical', 'true'); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 23, 'M', 'black', 'quiet', 'false'); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melisa', 65, 'F', 'white', 'nostalgic', 'true'); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 45, 'M', 'white', 'irritable', 'false'); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 15, 'F', 'black', 'social', 'true');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 65, 'F', 'black', 'optimistic', 'true');
